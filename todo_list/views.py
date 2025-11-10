d8545edf83f5f47e57d4508475445f50c6dee2bb
stackbento <shyam1024d@gmail.com>
2025-11-11 00:38:45 +0100

    django messages successful

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import List
 from . forms import ListForm
-
+from django.contrib import messages
 # Create your views here.
 def home(request):
-    #access the objects in the List(from models.py/Database)
-    all_items = List.objects.all()
-    #convention : context dictionary to access the objects from 'all_items'
-    context = {'all_items':all_items}
-    return render(request, 'home.html', context)
+    if request.method =='POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items=List.objects.all()
+            messages.success(request, ('Item has been added to List!'))
+            return redirect('home')
+    else:        
+        #access the objects in the List(from models.py/Database)
+        all_items = List.objects.all()
+        #convention : context dictionary to access the objects from 'all_items'
+        context = {'all_items':all_items}
+        return render(request, 'home.html', context)
 
 
 def about(request):
